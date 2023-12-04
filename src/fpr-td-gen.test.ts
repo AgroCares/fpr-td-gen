12718b30f2348b1e924875c230f7ddd53a1c2df8
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2023-12-04 17:23:06 +0100

    Add getTd to return the technical documentation

--- src/fpr-td-gen.test.ts
+++ src/fpr-td-gen.test.ts
@@ -5,3 +5,17 @@ test('Assign name to product', () => {
   const fprTdGen = new FprTdGen('MyProduct')
   expect(fprTdGen.getName()).toBe('MyProduct')
 })
+
+test('Assign name to product', () => {
+  const fprTdGen = new FprTdGen('MyProduct2')
+  expect(fprTdGen.getName()).toBe('MyProduct2')
+})
+
+test('Return the technical documentation of the FPR', () => {
+  const fprTdGen = new FprTdGen('My FPR')
+  const td = fprTdGen.getTd()
+  expect(td).toEqual({
+    name: 'My FPR',
+    version: 'FPR/2022'
+  })
+})
--- src/fpr-td-gen.ts
+++ src/fpr-td-gen.ts
@@ -7,6 +7,13 @@ class FprTdGen {
   getName (): string {
     return this.name
   }
+
+  getTd (): object {
+    return {
+      name: this.name,
+      version: 'FPR/2022'
+    }
+  }
 }
 
 export default FprTdGen
