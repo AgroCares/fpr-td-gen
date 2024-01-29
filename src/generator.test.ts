45ee9ef0765019528f2e31f3ecc2d184d498f1d5
BrentWHR <brent.riechelman@xs4all.nl>
2024-01-29 13:18:27 +0100

    rm commented out code

--- src/generator.test.ts
+++ src/generator.test.ts
@@ -2,8 +2,6 @@ import { describe, it, expect } from 'vitest'
 
 import Generator from './generator.ts'
 
-/** import \{ cmcDesginations, idTypes, pfcDesignations \} from './shared.types' */
-
 describe('Generator', () => {
   it('should create a generator with the given locale', () => {
     const generator = new Generator('en')
@@ -25,41 +23,6 @@ describe('Generator', () => {
       options: null
     })
   })
-  /**
- *   it('should update the previous questionId', () =\> \{
-    const generator = new Generator('en')
-
-    generator.previousQuestionId = undefined
-
-    generator.getNextQuestion()
-
-    expect(generator.previousQuestionId).toBe('Q1')
-
-    /** Iterate one more time */
-  /** generator.getNextQuestion()
-
-    expect(generator.previousQuestionId).toBe('Q2')
-  \})
- */
-
-  /**
- * Test for a more elaborate identifyNextQuestion method which will follol in another PR
- *  describe('Generator', () =\> \{
-    it('should not throw an error for any combination of previousQuestionId, pfcDesgination, and cmcDesignation', () =\> \{
-      const generator = new Generator('en')
-      const previousQuestionIds = idTypes
-      const aPfcDesignation = pfcDesignations
-      const aCmcDesignations = cmcDesginations
-      for (const previousQuestionId of previousQuestionIds) \{
-        for (const pfcDesgination of aPfcDesignation) \{
-          for (const cmcDesignation of aCmcDesignations) \{
-            expect(() =\> generator.identifyNextQuestion(previousQuestionId, pfcDesgination, cmcDesignation)).not.toThrow()
-          \}
-        \}
-      \}
-    \})
-  \})
- */
 
   it('should get the technical documentation', () => {
     const generator = new Generator('en')
