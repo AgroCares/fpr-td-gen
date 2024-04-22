f4859a715c0abec33ea6601a976de238eb5e20b3
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2024-04-22 09:58:01 +0200

    Update changelog and bump version number

--- src/generator.ts
+++ src/generator.ts
@@ -16,7 +16,7 @@ class Generator {
   * @returns A Generator class with the functions {@link getNextQuestion} and {@link getTechnicalDocumentation}
   * @public
   */
-  packageVersion = '1.0.0'
+  packageVersion = '1.1.0'
   locale: localesType
   fprVersion: fprVersionType
   pfcDesignation: pfcType = undefined
